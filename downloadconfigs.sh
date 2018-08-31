ccbcae978d94acf775bfd212788607ce0450cbff
tparker <tparker@usgs.gov>
2018-08-31 08:58:08 -0800

    add auth to config download

--- downloadconfigs.sh
+++ downloadconfigs.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-wget -O /tmp/wrt.yaml $WRP_CONFIG_URL
+WGET="wget -O /tmp/wrt.yaml"
+if [ "x$HTTP_USER" != x ]; then
+    WGET="${WGET} --http-user=$HTTP_USER --http-passwd=$HTTP_PASSWD --auth-no-challenge"
+fi
+
+$WGET $WRP_CONFIG_URL
