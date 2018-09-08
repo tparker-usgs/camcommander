f9ab06412f692eee073926f403153937727a46b6
tparker <tparker@usgs.gov>
2018-09-07 17:31:25 -0800

    move configupdater to tomputils

--- downloadconfigs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-WGET="wget -O /tmp/wrp.yaml"
-if [ "x$HTTP_USER" != x ]; then
-    WGET="${WGET} --http-user=$HTTP_USER --http-passwd=$HTTP_PASSWD --auth-no-challenge"
-fi
-
-$WGET $WRP_CONFIG_URL
