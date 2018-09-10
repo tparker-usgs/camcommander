7472f4b82600c317fe1dd055f6495dee1cdf68ee
tparker <tparker@usgs.gov>
2018-09-10 09:47:31 -0800

    move user home dir to svn dir

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 groupadd -g $CAMCOMMANDER_GROUPID camcmdr
-useradd -u $CAMCOMMANDER_USERID -g camcmdr -d /app/camcommander camcmdr
+useradd -u $CAMCOMMANDER_USERID -g camcmdr -d /tmp/camcommander/svn camcmdr
 su --preserve-environment camcmdr -c "/usr/local/bin/supercronic /app/camcommander/cron-camcommander"
