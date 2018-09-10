210f905725d1ea53909d931dd7a3d59d5d6e68c1
tparker <tparker@usgs.gov>
2018-09-09 16:53:27 -0800

    tweak launch.sh

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 
 groupadd -g $CAMCOMMANDER_GROUPID camcmdr
 useradd -u $CAMCOMMANDER_USERID -g camcmdr -d /app/camcommander camcmdr
-su - camcmdr -c /usr/local/bin/supercronic /app/camcommander/cron-camcommander
+su - camcmdr -c "/usr/local/bin/supercronic /app/camcommander/cron-camcommander"
