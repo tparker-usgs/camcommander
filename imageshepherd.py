f0a6a27b5b5761c782634b7e21f4f6c2c3179ac1
tparker <tparker@usgs.gov>
2018-09-10 17:50:10 -0800

    flake8

--- imageshepherd.py
+++ imageshepherd.py
@@ -42,7 +42,8 @@ def get_new_images(config):
             new_image_count += 1
         else:
             logger.debug("yada yada yada %s", line)
-    logger.debug("All done with %s, new images: %d", config['name'], new_image_count)
+    logger.debug("All done with %s, new images: %d", config['name'],
+                 new_image_count)
 
     return new_image_count
 
