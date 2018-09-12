3be0c639a4253af3b3af628a8a35b577b6aaa8a8
tparker <tparker@usgs.gov>
2018-09-11 18:19:14 -0800

    fix logging

--- imageshepherd.py
+++ imageshepherd.py
@@ -52,7 +52,7 @@ def flush_old_images(config):
     ssh_cmd = 'ssh {} "find {} -name *.jpg -ctime +{} -print -exec rm {{}} \;"'
     ssh_cmd = ssh_cmd.format(config['name'], config['path'],
                              config['retention'])
-    logger.info("Flushing old images with: {}", ssh_cmd)
+    logger.info("Flushing old images with: %s", ssh_cmd)
     os.system(ssh_cmd)
 
 
