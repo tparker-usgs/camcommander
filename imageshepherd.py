33413f4614d49623c3fbdcca72cd325ec5ac3c5f
tparker <tparker@usgs.gov>
2018-09-11 17:57:25 -0800

    testing retention

--- imageshepherd.py
+++ imageshepherd.py
@@ -52,8 +52,7 @@ def flush_old_images(config):
     ssh_cmd = 'ssh {} "find {} -name *.jpg -ctime +{} -exec echo {{}} \;"'
     ssh_cmd = ssh_cmd.format(config['name'], config['path'],
                              config['retention'])
-    logger.debug("ssh: %s", ssh_cmd)
-    #os.system(ssh_cmd)
+    os.system(ssh_cmd)
 
 
 def deliver_images(config):
